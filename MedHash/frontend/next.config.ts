12b7f3b26929065649c323401eaac7f218fb82bc
thecodermaniac <aritra.mondal.tech@gmail.com>
2026-03-08 10:25:20 +0000

    made to ssr

--- MedHash/frontend/next.config.ts
+++ MedHash/frontend/next.config.ts
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Add this for static export
+  output: 'standalone',  // Change from 'export' to 'standalone'
   images: {
-    unoptimized: true, // Required for static export
+    unoptimized: true,
   },
   trailingSlash: true,
 };
